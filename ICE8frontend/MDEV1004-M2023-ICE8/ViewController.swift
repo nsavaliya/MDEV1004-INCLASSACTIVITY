7d2cdf6365e1f85e9268adf6ef7da3dc144ff996
Namrata Savaliya <savaliyanamrata2002@gmail.com>
2023-07-23 17:38:49 -0400

    added files for ICE8

---
+++ ICE8frontend/MDEV1004-M2023-ICE8/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MDEV1004-M2023-ICE8
+//
+//  Created by Namrata Savaliya on 2023-07-22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
